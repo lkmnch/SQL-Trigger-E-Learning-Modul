3580f2e287f3e3ffc492065593fe588b9db0a4dd
Loukmane Chaou <72920928+lkmnch@users.noreply.github.com>
2024-01-23 02:04:28 +0100

    Implemented Styling

--- frontend/sql-trigger-e-learning-modul/src/App.jsx
+++ frontend/sql-trigger-e-learning-modul/src/App.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react"
 import axios from "axios"
+import { Database } from "lucide-react"
+import { Sidebar } from "flowbite-react"
+import { HiShoppingBag, HiDatabase } from "react-icons/hi"
+import { DarkThemeToggle } from "flowbite-react"
+import { useEffect } from "react"
 
 const baseURL = "http://localhost:3000/plants"
 
@@ -11,6 +16,10 @@ function App() {
 	const handleQueryChange = (event) => {
 		setQuery(event.target.value)
 	}
+
+	useEffect(() => {
+		//TODO Create API-Endpoint in Backend for a GET-Request. The API Responds with all Tables in the Database (Show Tables)
+	})
 
 	const handleExecuteQuery = () => {
 		axios({
@@ -33,54 +42,81 @@ function App() {
 				setData([])
 			}
 		})
-
-		// fetch get:
 	}
 	return (
-		<>
-			<header>
-				<h1>SQL-Trigger, E-Learning-Modul</h1>
+		<div className='bg-slate-50 dark:bg-slate-600 min-h-screen'>
+			<header className='container mx-auto'>
+				<h1 className='mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white'>
+					SQL-Trigger, E-Learning-Modul
+				</h1>
+				<DarkThemeToggle />
 			</header>
-			<main className='bg-slate-400'>
-				<textarea
-					value={query}
-					onChange={handleQueryChange}
-					placeholder='Hier Query eingeben'
-					rows={20}
-					cols={100}
-				/>
-				<button className='bg-lime-500' onClick={handleExecuteQuery}>
-					Run
-				</button>
+			<div className='flex container mx-auto'>
+				<div className='container w-1/3'>
+					<Sidebar aria-label='Sidebar with multi-level dropdown example'>
+						<Sidebar.Items>
+							<Sidebar.ItemGroup>
+								<Sidebar.Collapse icon={HiDatabase} label='Database'>
+									<Sidebar.Item href='#'>Products</Sidebar.Item>
+								</Sidebar.Collapse>
+							</Sidebar.ItemGroup>
+						</Sidebar.Items>
+					</Sidebar>
+				</div>
+				<div className='container mx-auto flex-row gap-7'>
+					<main>
+						<textarea
+							className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+							value={query}
+							onChange={handleQueryChange}
+							placeholder='Hier Query eingeben'
+							rows={20}
+							cols={100}
+						/>
+						<button
+							className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+							onClick={handleExecuteQuery}>
+							Run
+						</button>
 
-				{data ? (
-					<div id='select-output'>
-						<table>
-							<thead>
-								<tr>
-									{tableHeaders.map((header, index) => (
-										<th key={index}>{header}</th>
-									))}
-								</tr>
-							</thead>
-							<tbody>
-								{data.map((row, index) => {
-									return (
-										<tr key={index}>
-											{Object.values(row).map((rowValue, index) => {
-												return <td key={index}>{rowValue}</td>
-											})}
+						{data ? (
+							<div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
+								<table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
+									<thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+										<tr>
+											{tableHeaders.map((header, index) => (
+												<th scope='col' className='px-6 py-3' key={index}>
+													{header}
+												</th>
+											))}
 										</tr>
-									)
-								})}
-							</tbody>
-						</table>
-					</div>
-				) : (
-					<div></div>
-				)}
-			</main>
-		</>
+									</thead>
+									<tbody>
+										{data.map((row, index) => {
+											return (
+												<tr
+													className='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'
+													key={index}>
+													{Object.values(row).map((rowValue, index) => {
+														return (
+															<td className='px-6 py-4' key={index}>
+																{rowValue}
+															</td>
+														)
+													})}
+												</tr>
+											)
+										})}
+									</tbody>
+								</table>
+							</div>
+						) : (
+							<div></div>
+						)}
+					</main>
+				</div>
+			</div>
+		</div>
 	)
 }
 
